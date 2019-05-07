d2f92f90321df297e910a1bbf41394bbd33ec7c0
Toph Allen <toph.allen@gmail.com>
2019-05-07 16:08:24 -0400

    Added package-level documentation.

---
+++ R/yenpathy-package.R
@@ -0,0 +1,10 @@
+#' yenpathy: Quickly Find K Shortest Paths Through a Weighted Graph
+#'
+#' Using yenpathy, you can quickly find k shortest paths through a
+#' weighted graph. See GitHub for more details at
+#' \url{https://github.com/ecohealthalliance/yenpathy}.
+#'
+#' @docType package
+#' @name yenpathy
+#' @keywords internal
+NULL
--- tests/testthat/test-return-correct-paths.R
+++ tests/testthat/test-return-correct-paths.R
@@ -33,7 +33,17 @@ test_that("correct number of paths are returned", {
   expect_equal(map_int(paths, length), c(1, 2, 3, 4, 4))
 })
 
-test_that("edge penalty correctly changes returned path", {
+test_that("edge penalty correctly changes returned paths", {
   paths <- k_shortest_paths(weighted_graph, start_node, end_node, edge_penalty = 1)
   expect_equal(paths[[1]], c(6, 3, 2))
+})
+
+test_that("disconnected graphs returns an empty list", {
+  disconnected_graph <- data.frame(
+    source = c(6, 1, 8, 6, 6, 3, 6, 4, 5, 7, 9),
+    sink = c(1, 8, 2, 2, 3, 2, 4, 5, 7, 2, 10),
+    weight = c(1, 1, 1, 5, 1.5, 2, 1, 0.5, 0.5, 0.5, 1)
+  )
+
+  expect_equal(k_shortest_paths(disconnected_graph, 1, 10), list())
 })
